78aacd902520b31f4b55ab822955a43e0db57a32
정명지 <mnn01400@gmail.com>
2019-11-23 15:16:23 +0900

    second commit

---
+++ src/js/circle.js
@@ -0,0 +1,4 @@
+// named export
+export const area = r => Math.PI * r * r;
+
+export const circumfefence = r => Math.PI * r * 2;
---
+++ src/js/cube.js
@@ -0,0 +1,6 @@
+// 전체 모됼화 => es6 default export
+const cube = {
+	volume: x => x * x * x,
+	b: (r) => r * r
+}
+export default cube;
